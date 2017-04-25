9ef76bf91a7334bdf852d6236a54eedbd63b5d46
Joshua Backfield <suroot.chmod777@gmail.com>
2017-04-24 22:42:24 -0400

    Fixing issue scala-library-all vs. scala-dist for a dependency. Also migrated all DB scripts into the heystk-db module. Should make it simpler to do a DDL.

--- system-ui/src/main/scala/com/fustigatedcat/heystk/ui/model/User.scala
+++ system-ui/src/main/scala/com/fustigatedcat/heystk/ui/model/User.scala
@@ -1,9 +1,12 @@
 package com.fustigatedcat.heystk.ui.model
 
 import org.squeryl.KeyedEntity
+import org.squeryl.annotations.Column
 
 case class User(id : Long,
                 username : String,
+                @Column("first_name") firstName : String,
+                @Column("last_name") lastName : String,
                 salt : String,
                 hash : String) extends KeyedEntity[Long] {
 
